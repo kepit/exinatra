ab220bb9d911cb2b118c78c2b92e66d9609f2179
Jussi Uusi-Illikainen <jui@kepit.fi>
2014-09-20 19:35:53 +0300

    indent fix

--- lib/exinatra/router.ex
+++ lib/exinatra/router.ex
@@ -1,6 +1,5 @@
 defmodule Exinatra.Router do
-
-	defmacro __using__(_) do
+  defmacro __using__(_) do
     quote do
       import unquote(__MODULE__)
       import Plug.Conn
@@ -10,62 +9,62 @@ defmodule Exinatra.Router do
       plug Plug.Parsers, parsers: [:urlencoded, :multipart]
 
       plug :match
-			plug :dispatch
+      plug :dispatch
 
-			def dispatch(%Plug.Conn{assigns: assigns} = conn, _opts) do
-				conn = call_before_filters(conn)
-				conn = Map.get(conn.private, :plug_route).(conn)
-				conn = call_after_filters(conn)
-				conn
-			end
+      def dispatch(%Plug.Conn{assigns: assigns} = conn, _opts) do
+        conn = call_before_filters(conn)
+        conn = Map.get(conn.private, :plug_route).(conn)
+        conn = call_after_filters(conn)
+        conn
+      end
 
-			def start() do
-				IO.puts "Running with Cowboy on http://localhost:4000"
-				Plug.Adapters.Cowboy.http __MODULE__, [], [{:port, 8080}]
-			end
+      def start() do
+        IO.puts "Running with Cowboy on http://localhost:4000"
+        Plug.Adapters.Cowboy.http __MODULE__, [], [{:port, 8080}]
+      end
     end
   end
 
-	defmacro __before_compile__(env) do
-		module = env.module
+  defmacro __before_compile__(env) do
+    module = env.module
 
     quote do
       Plug.Router.match _ do
         conn = var!(conn)
-				send_resp(conn, 404, "Not found")
+        send_resp(conn, 404, "Not found")
       end
 
-			defp call_before_filters(%Plug.Conn{state: :unset} = conn) do
-				if function_exported?(__MODULE__, :before_filter_fun,0) do
-					conn = before_filter_fun().(conn)
-				end
-				conn
-			end
+      defp call_before_filters(%Plug.Conn{state: :unset} = conn) do
+        if function_exported?(__MODULE__, :before_filter_fun,0) do
+          conn = before_filter_fun().(conn)
+        end
+        conn
+      end
 
-			defp call_after_filters(%Plug.Conn{} = conn) do
-				if function_exported?(__MODULE__, :after_filter_fun,0) do
-					conn = after_filter_fun().(conn)
-				end
-				conn
-			end
+      defp call_after_filters(%Plug.Conn{} = conn) do
+        if function_exported?(__MODULE__, :after_filter_fun,0) do
+          conn = after_filter_fun().(conn)
+        end
+        conn
+      end
 
-		end
+    end
   end
 
-	defmacro before_filter(expression) do
-		quote do
-			def before_filter_fun() do
-				fn var!(conn) -> unquote(expression[:do]) end
-			end
-		end
-	end
+  defmacro before_filter(expression) do
+    quote do
+      def before_filter_fun() do
+        fn var!(conn) -> unquote(expression[:do]) end
+      end
+    end
+  end
 	
-	defmacro after_filter(expression) do
-		quote do
-			def after_filter_fun() do
-				fn var!(conn) -> unquote(expression[:do]) end
-			end
-		end
-	end
-  
+  defmacro after_filter(expression) do
+    quote do
+      def after_filter_fun() do
+        fn var!(conn) -> unquote(expression[:do]) end
+      end
+    end
+  end
+
 end
