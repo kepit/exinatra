61a8a6d869218dd6cd16dcd1f5d2c0ff7d218823
Jussi Uusi-Illikainen <jui@kepit.fi>
2014-09-20 17:45:57 +0300

    Filter fixes

--- lib/exinatra/router.ex
+++ lib/exinatra/router.ex
@@ -13,9 +13,9 @@ defmodule Exinatra.Router do
 			plug :dispatch
 
 			def dispatch(%Plug.Conn{assigns: assigns} = conn, _opts) do
-				before_filter_fun()
+				call_before_filters(conn)
 				ret = Map.get(conn.private, :plug_route).(conn)
-				after_filter_fun()
+				call_after_filters(conn)
 				ret
 			end
 
@@ -24,31 +24,38 @@ defmodule Exinatra.Router do
 				Plug.Adapters.Cowboy.http __MODULE__, []
 			end
 
-			before_filter do
-			end
-			
-			after_filter do
-			end
-			
-			defoverridable [before_filter_fun: 0, after_filter_fun: 0]
-			
     end
   end
 
-	defmacro __before_compile__(_) do
+	defmacro __before_compile__(env) do
+		module = env.module
+
     quote do
       Plug.Router.match _ do
         conn = var!(conn)
 				send_resp(conn, 404, "Not found")
       end
-			
-    end
+
+			defp call_before_filters(%Plug.Conn{state: :unset} = conn) do
+				try do
+					before_filter_fun().(conn)
+				end
+			end
+
+			defp call_after_filters(%Plug.Conn{state: :unset} = conn) do
+				try do
+					after_filter_fun().(conn)
+				end
+			end
+
+		end
+		
   end
 
 	defmacro before_filter(expression) do
 		quote do
 			def before_filter_fun() do
-				unquote(expression)
+				fn var!(conn) -> unquote(expression[:do]) end
 			end
 		end
 	end
@@ -56,7 +63,7 @@ defmodule Exinatra.Router do
 	defmacro after_filter(expression) do
 		quote do
 			def after_filter_fun() do
-				unquote(expression)
+				fn var!(conn) -> unquote(expression[:do]) end
 			end
 		end
 	end
