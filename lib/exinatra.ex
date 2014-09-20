c0c1b21fc30d5a7e1f2e4d272202134bd1b00a09
Jussi Uusi-Illikainen <jui@kepit.fi>
2014-09-20 19:36:26 +0300

    indent fix

--- lib/exinatra.ex
+++ lib/exinatra.ex
@@ -1,8 +1,7 @@
 defmodule Exinatra do
-	defmacro __using__(_) do
-		quote do
-			use Exinatra.Router
-		end
-	end
-
+  defmacro __using__(_) do
+    quote do
+      use Exinatra.Router
+    end
+  end
 end
